2a4c098f67bdee2365a4f93382d1d29c8bdb8fe8
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2023-03-17 00:48:01 -0400

    Added Loading data from the csv to the database

--- Project Files/MongoDBConnection.py
+++ Project Files/MongoDBConnection.py
@@ -3,10 +3,17 @@ import os
 from pymongo import MongoClient
 load_dotenv(find_dotenv())
 
-password = os.environ.get("MONGODB_PWD")
-
-connection_string = f"mongodb+srv://SebastienRamsay:{password}@mallshopdb.jn1kwwg.mongodb.net/?retryWrites=true&w=majority"
 
 def connect():
-    client = MongoClient(connection_string)
+    
+    password = os.environ.get("MONGODB_PWD")
+
+    connection_string = f"mongodb+srv://SebastienRamsay:{password}@mallshopdb.jn1kwwg.mongodb.net/?retryWrites=true&w=majority"
+    
+    client = MongoClient(connection_string, tlsAllowInvalidCertificates=True)
+    
+    databases = client.list_database_names()
+    
+    collections = client.Practical_Project_Part_3.list_collection_names()
+
     return client
--- Project Files/PotatoController.py
+++ Project Files/PotatoController.py
@@ -3,15 +3,14 @@ from PotatoModel import PotatoDTO
 import os
 from MongoDBConnection import connect
 
-
-# Connecting to the database
+#Connect to DB
 with connect() as client:
-    db = client.Practical_Project_Part_3
-    collection = db.Potato_Info
-
+    db = client.Practical_Project_Part_3.Potato_Info
 
 # Making a new array to store DTO's
 potatos = []
+
+dbPotatos = []
                 
 # Hardcoding the column names into the program as an array
 columnNames = ["ID", "REF_DATE","GEO","DGUID","Area, production and farm value of potatoes","UOM","UOM_ID","SCALAR_FACTOR","SCALAR_ID","VECTOR","COORDINATE","VALUE","STATUS","SYMBOL","TERMINATED","DECIMALS"]
@@ -27,7 +26,7 @@ def deleteSelected(selectedPotatos):
     for potato in selectedPotatos:
         potatos.remove(potato)
         
-#updates the selected potato
+#updates the selected potato by
 def updatePotatos(updatedPotatos):
     """replace in memory potatos with updatedPotatos
 
@@ -98,6 +97,11 @@ def loadData():
     """
     #clear in memory data
     potatos.clear()
+    
+    #Connect to DB
+    with connect() as client:
+        db = client.Practical_Project_Part_3.Potato_Info
+        db.delete_many({})
 
     # Try statement for exeption handling
     try: 
@@ -105,7 +109,7 @@ def loadData():
             # Reading the CSV file from the project directory.
             # irst arg is file name, second arg indicates to perform a read operation.
             # "as" indicates the name to give this file you are opening
-        with open ('32100358.csv', 'r') as csv_file:
+        with open ('Project Files/32100358.csv', 'r') as csv_file:
 
             # Make a new variable called csv_reader that 
             # uses the csv import to use the reader function
@@ -116,21 +120,21 @@ def loadData():
             next(csv_reader)
                 
             numberOfPotatos = 0
-
-            # Itterating through the lines in the csv file called csv_reader, each line is accessed with "line"
-            for line in csv_reader:
-                if numberOfPotatos < 100:
-                    # Make a new variable called "potato" 
-                    # and set it to our DTO "PotatoDTO" 
-                    # and populate the DTO with the information gathered from our current row "Line"
-                    potato = PotatoDTO(numberOfPotatos, line[0], line[1], line[2], line[3], line[4],\
-                        line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14])
-                    
-                    # Adding the current potato object(DTO) to the potatos array(Array of DTOS's)
-                    potatos.append(potato)
-                             
-                    numberOfPotatos += 1       
-        return ("Reloaded data into memory")        
+            try:
+                # Itterating through the lines in the csv file called csv_reader, each line is accessed with "line"
+                for line in csv_reader:
+                    if numberOfPotatos < 100:
+                        line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14]
+                        dbPotato = {"_id": numberOfPotatos,"REF_DATE": line[0],"GEO": line[1],"DGUID": line[2], "area, production and farm value of potatoes": line[3],"UOM": line[4],"UOM_ID": line[5],"SCALAR_FACTOR": line[6],"SCALAR_ID": line[7],"VECTOR": line[8],"COORDINATE": line[9],"VALUE": line[10],"STATUS": line[11],"SYMBOL": line[12],"TERMINATED": line[13],"DECIMALS": line[14]}
+                        dbPotatos.append(dbPotato)
+                        numberOfPotatos += 1 
+                #Connect to DB
+                with connect() as client:
+                    db = client.Practical_Project_Part_3.Potato_Info        
+                    db.insert_many(dbPotatos) 
+            except Exception as e:
+                return("An exception occurred: ", e)
+        return ("Reloaded data into memory")      
     # Except statment will catch any exceptions that take place
     except FileNotFoundError:
         #PotatoView.print("FILE NOT FOUND!")
